b2ac0b96a87da04483d6db203bd4cbd512a3fbb4
Joe2010x <64711803+Joe2010x@users.noreply.github.com>
2022-02-07 22:37:22 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ const Header = () =>{
 
     return (
         <div  className = "header_bar">
-            <img id="headerLogo" src={logo} alt="Logo" />
-            <p>Fullstack development course final project ---- Lyyti VP project</p> 
+        {/*<img id="headerLogo" src={logo} alt="Logo" /> */}
+            <p>VP project</p> 
             <div className="line"></div>
         </div>
     )
