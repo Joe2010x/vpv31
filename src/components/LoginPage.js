636a1310ee91e194687cec0733818cf07c7800d5
Dongping Joe Sun <64711803+Joe2010x@users.noreply.github.com>
2022-08-30 23:07:31 +0200

    Update LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 const LoginPage =(props)=>{
-    //const base_Url = "http://192.168.10.197:3001"
     const base_Url = "https://vpbackend-utu.herokuapp.com"
     const [email,setEmail] = useState ("")
     const [userList,setUserList]=useState(null)
